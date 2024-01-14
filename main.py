9d896c8834339502fb403f2a532895cb439121a6
Muhammad Habil Amardias <habilamar@gmail.com>
2024-01-14 13:12:20 +0700

    add cosine scheduler

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def mse(y:np.ndarray,ycap:np.ndarray):
     assert y.shape==ycap.shape,"y and ycap must have same dimension"
@@ -15,12 +16,18 @@ def mse_grad(y:np.ndarray,ycap:np.ndarray,exog:np.ndarray):
     grads_b=(2/len(y))*np.sum(np.subtract(ycap,y))
     return grads_b,grads_w
 
+def CosineScheduler(step:int,max_step:int,base_lr:float,final_lr:float):
+    lr=final_lr+(base_lr-final_lr)*(1+math.cos(math.pi*step/max_step))/2
+    return lr
 class GDLinearRegression():
-    def __init__(self,lr:float=1e-1,max_iter:int=10,verbose:int=1):
+    def __init__(self,base_lr:float=3e-1,final_lr:float=1e-3,max_iter:int=100,verbose:int=1,cosine:bool=False):
         assert verbose==0 or verbose==1, "verbose must only 0 or 1"
-        self.lr=lr
+        assert final_lr<base_lr, "Final LR must be smaller than Base LR"
+        self.base_lr=base_lr
+        self.final_lr=final_lr
         self.max_iter=max_iter
         self.verbose=verbose
+        self.cosine=cosine
         self.params=None
     def fit(self,endog:np.ndarray,exog:np.ndarray):
         assert len(endog.shape)==1,"y must be 1 dimension"
@@ -31,17 +38,19 @@ class GDLinearRegression():
         else:
             W:np.ndarray=np.random.randn(exog.shape[1]+1)
         iter=0
+        lr=self.base_lr
         while iter<self.max_iter:
             ycap=exog@W[1:]+W[0]
-            if self.verbose==1:
-                print(f'Iteration {iter} Error: ',mse(endog,ycap))
             grad_b,grad_w=mse_grad(endog,ycap,exog)
-            W[1:]=W[1:]-self.lr*grad_w
-            W[0]=W[0]-self.lr*grad_b
+            W[1:]=W[1:]-lr*grad_w
+            W[0]=W[0]-lr*grad_b
+            ycap1=exog@W[1:]+W[0]
+            if self.verbose==1:
+                print(f'Iteration {iter+1} MSE with lr {lr}: ',mse(endog,ycap1))
+            if self.cosine==True:
+                lr=CosineScheduler(iter,self.max_iter,self.base_lr,self.final_lr)
             iter+=1
         self.params=W
-        error=mse(endog,exog@W[1:]+W[0])
-        print(f"Last Iteration Error: ", error)
     def predict(self,exog:np.ndarray):
         assert len(exog.shape)<=2,"x must be either 1d or 2d"
         assert exog.shape[1]+1==self.params.shape[0],"features doesn't match with params"
@@ -49,8 +58,8 @@ class GDLinearRegression():
 
 #testing     
 if __name__=='__main__':
-    model=GDLinearRegression(verbose=1,max_iter=50)
-    X=np.random.randn(100,5)
+    X=np.random.rand(100,5)
     y=np.random.rand(100)
+    model=GDLinearRegression(verbose=1,max_iter=10,cosine=True)
     model.fit(y,X)
     print(mse(y,model.predict(X)))
